a905710286bf5786d5dc9eeb45f200e56361dc83
Abdumalik <48346947+python-pro@users.noreply.github.com>
2019-04-07 13:38:31 -0500

    deleted unnecessary line

--- edabit_codes.py
+++ edabit_codes.py
@@ -9,7 +9,7 @@ Write a function that classifies the missing angle as either "acute", "right", o
 An acute angle is one smaller than 90 degrees.
 A right angle is one that is exactly 90 degrees.
 An obtuse angle is one greater than 90 degrees (but smaller than 180 degrees).
-For example: missing_angle(11, 20) should return "obtuse", since the missing angle would be 149 degrees, which makes it obtuse.
+
 """
 
 
