40fbd21b6ee620f2b2ce37abe946bee6f3d7e7de
Abdumalik <48346947+python-pro@users.noreply.github.com>
2019-04-07 14:10:09 -0500

    sorting in ascending order code added

--- edabit_codes.py
+++ edabit_codes.py
@@ -40,3 +40,13 @@ def find_digit_amount(num):
   string = str(num)
   length = len(string)
   return length
+
+
+"""Create a function that takes a list of numbers and returns a new list, sorted in ascending order (smallest to biggest).
+
+Sort numbers list in ascending order.
+If function's argument is an empty list, return an empty list.
+Return a new list of sorted numbers."""
+
+def sortNumsAscending(lst):
+	return sorted(lst)
