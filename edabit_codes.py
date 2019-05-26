3591f6fc9d7b28dbab1d9abf94e19547e78aaad1
Abdumalik <48346947+python-pro@users.noreply.github.com>
2019-05-26 08:27:50 -0500

    Update edabit_codes.py

--- edabit_codes.py
+++ edabit_codes.py
@@ -2,6 +2,7 @@
 #MSAW
 
 """
+1)
 You are given 2 out of 3 of the angles in a triangle, in degrees.
 
 Write a function that classifies the missing angle as either "acute", "right", or "obtuse" based on its degrees.
@@ -13,14 +14,8 @@ An obtuse angle is one greater than 90 degrees (but smaller than 180 degrees).
 """
 
 
-def missing_angle(angle1, angle2):
-	if 180-(angle1+angle2)<90:
-		return "acute"
-	elif 180-(angle1+angle2)==90:
-		return "right"
-	else:
-		return "obtuse"
 
+2)
 	
 """
 Create a function that takes a number as an argument and returns the amount of digits it has.
@@ -35,18 +30,95 @@ find_digit_amount(7154) ➞ 4
 find_digit_amount(61217311514) ➞ 11
 
 find_digit_amount(0) ➞ 1
-"""
-def find_digit_amount(num):
-  string = str(num)
-  length = len(string)
-  return length
 
+"""
+
+3)
 
 """Create a function that takes a list of numbers and returns a new list, sorted in ascending order (smallest to biggest).
 
 Sort numbers list in ascending order.
 If function's argument is an empty list, return an empty list.
-Return a new list of sorted numbers."""
+Return a new list of sorted numbers.
 
+"""
+
+
+4)
+"""
+Create a function that takes a single string as an argument and returns an ordered list containing the indexes of all capital letters in the string.
+Examples
+indexOfCaps("eDaBiT") ➞ [1, 3, 5]
+
+indexOfCaps("eQuINoX") ➞ [1, 3, 4, 6]
+
+indexOfCaps("determine") ➞ []
+
+indexOfCaps("STRIKE") ➞ [0, 1, 2, 3, 4, 5]
+
+indexOfCaps("sUn") ➞ [1]
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+1)
+"""
+def missing_angle(angle1, angle2):
+	if 180-(angle1+angle2)<90:
+		return "acute"
+	elif 180-(angle1+angle2)==90:
+		return "right"
+	else:
+		return "obtuse"
+	
+"""
+
+2)
+"""
+def find_digit_amount(num):
+  string = str(num)
+  length = len(string)
+  return length
+  
+"""
+
+3)
+"""
 def sortNumsAscending(lst):
 	return sorted(lst)
+"""
+
+4)
+"""
+
+a)
+def indexOfCaps(word):
+	return [i for i in range(len(word)) if word[i].isupper()]
+b)
+result=list()
+for i in word:
+   if i.isupper():
+      result.append(word.index(i))
+return result
+"""
+
